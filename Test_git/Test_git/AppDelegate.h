44ab56e5b39577d84517599b2bf5d1b3305e0b8e
靳晓铁 <jinxiaotie@jinxiaotiedeMacBook-Pro.local>
2017-07-09 21:15:11 +0800

    add project Test_git

---
+++ Test_git/Test_git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test_git
+//
+//  Created by 靳晓铁 on 2017/7/9.
+//  Copyright © 2017年 Tim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test_git/Test_git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test_git
+//
+//  Created by 靳晓铁 on 2017/7/9.
+//  Copyright © 2017年 Tim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
